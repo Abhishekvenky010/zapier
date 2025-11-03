bdd5f12f1ffe2d197ed294044071916fca8d34f5
Abhishek <abhishekvenky010@gmail.com>
2025-11-04 01:17:20 +0530

    hooks done

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,39 @@
-import express from "express";
-import { PrismaClient } from "./generated/prisma";
+import express from "express"
+import {PrismaClient } from "@prisma/client";
+
 const client = new PrismaClient();
+
 const app = express();
-app.post("/hooks/catch/:userId/:ZapId",async (req,res)=>{
+app.use(express.json());
+
+// https://hooks.zapier.com/hooks/catch/17043103/22b8496/
+// password logic
+app.post("/hooks/catch/:userId/:zapId", async (req, res) => {
     const userId = req.params.userId;
-    const ZapId = req.params.ZapId;
+    const zapId = req.params.zapId;
     const body = req.body;
-    await client.$transaction(async tx=>{
-       const run = await client.zaprun.create({
-        data: { 
-            zapid:ZapId,
-            metadata:body
-         }
-    });
-     await client.zaprunoutofbox.create({
-        data:{
-            zaprunid:run.id
-        }
-     })
-     
-         
+
+    console.log("reaqched here");
+    // store in db a new trigger
+    await client.$transaction(async tx => {
+        console.log("reaqched here 2");
+        const run = await client.zapRun.create({
+            data: {
+                zapId: zapId,
+                metadata: body
+            }
+        });
+        console.log("reaqched here 3");
+
+        await client.zapRunOutbox.create({
+            data: {
+                zapRunId: run.id
+            }
+        })
     })
-    app.listen(3000);
-   
+    res.json({
+        message: "Webhook received"
+    })
 })
+
+app.listen(3000);
