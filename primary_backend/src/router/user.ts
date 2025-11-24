92568d1463e3662194a20f3d5f3e1e151d64255d
Abhishek <abhishekvenky010@gmail.com>
2025-11-24 23:55:46 +0530

    added primary_backend

--- primary_backend/src/router/user.ts
+++ primary_backend/src/router/user.ts
@@ -1,88 +1,103 @@
-import { NextFunction, Router } from "express";
+
+import { Router } from "express";
 import { authMiddleware } from "../middleware";
-import { SignupSchema,SigninSchema } from "../types";
+import { SigninSchema, SignupSchema } from "../types";
 import { prismaClient } from "../db";
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 import { JWT_PASSWORD } from "../config";
+
 const router = Router();
 
-router.post("signup",async (req,res)=>{
-   const body = req.body.username;
-   const parsedData = SignupSchema.safeParse(body);
+router.post("/signup", async (req, res) => {
+    const body = req.body;
+    const parsedData = SignupSchema.safeParse(body);
 
-      if (!parsedData.success) {
+    if (!parsedData.success) {
         console.log(parsedData.error);
         return res.status(411).json({
             message: "Incorrect inputs"
-        })}
-        const userExist = await prismaClient.user.findFirst({
-            where:{
-                email:parsedData.data?.username,
-            }
-        });
-        if(userExist){
-           return res.status(404).json({
-            msg:"user already exist"
-           })
-        }
-        await prismaClient.user.create({
-            data:{
-                email:parsedData.data?.username,
-                password:parsedData.data?.password,
-                name:parsedData.data?.name
-            }
         })
+    }
+
+    const userExists = await prismaClient.user.findFirst({
+        where: {
+            email: parsedData.data.username
+        }
+    });
+
+    if (userExists) {
+        return res.status(403).json({
+            message: "User already exists"
+        })
+    }
+
+    await prismaClient.user.create({
+        data: {
+            email: parsedData.data.username,
+            // TODO: Dont store passwords in plaintext, hash it
+            password: parsedData.data.password,
+            name: parsedData.data.name
+        }
+    })
+
+    // await sendEmail();
+
     return res.json({
-        message : "please verify ur account"
-    })
+        message: "Please verify your account by checking your email"
+    });
 
 })
-router.post("/signin",async (req,res)=>{
-  const body = req.body.username;
-  const parsedData = SigninSchema.safeParse(body);
-  if(!parsedData.success){
-    return res.status(411).json({
-        message:"incorrect Inputs"
-    })
-  }
-  const user= await prismaClient.user.findFirst({
-    where:{
-       email:parsedData.data.username,
-       password:parsedData.data.password
+
+router.post("/signin", async (req, res) => {
+    const body = req.body;
+    const parsedData = SigninSchema.safeParse(body);
+
+    if (!parsedData.success) {
+        return res.status(411).json({
+            message: "Incorrect inputs"
+        })
     }
-  });
-  if(!user){
-    return res.status(403).json({
-        msg:"user doesnt exist"
-    })
-  }
-  const token = jwt.sign({
-    id:user.id
-  },JWT_PASSWORD);
-  
+
+    const user = await prismaClient.user.findFirst({
+        where: {
+            email: parsedData.data.username,
+            password: parsedData.data.password
+        }
+    });
+    
+    if (!user) {
+        return res.status(403).json({
+            message: "Sorry credentials are incorrect"
+        })
+    }
+
+    // sign the jwt
+    const token = jwt.sign({
+        id: user.id
+    }, JWT_PASSWORD);
+
     res.json({
-        token:token,
-    })
-  
+        token: token,
+    });
 })
-router.get("/user",authMiddleware,async(req,res)=>{
+
+router.get("/", authMiddleware, async (req, res) => {
+    // TODO: Fix the type
     // @ts-ignore
-   const id : req.id;
-   const user= await prismaClient.user.findFirst({
-    where:{
-        id
-    },
-    select:{
-        name:true,
-        email:true
+    const id = req.id;
+    const user = await prismaClient.user.findFirst({
+        where: {
+            id
+        },
+        select: {
+            name: true,
+            email: true
+        }
+    });
 
-    }
-   });
-   return res.json({
-    user
-   });
-
+    return res.json({
+        user
+    });
 })
-
 
 export const userRouter = router;
