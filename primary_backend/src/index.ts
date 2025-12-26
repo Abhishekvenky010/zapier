3f5792fb0318a582025f1b66dfd3f0f8e30500ab
Abhishekvenky010 <abhishekvenky010@gmail.com>
2025-12-26 14:28:47 +0530

    added modals

--- primary_backend/src/index.ts
+++ primary_backend/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { userRouter } from "./router/user";
 import { zapRouter } from "./router/zap";
 import cors from "cors";
+import { triggerRouter } from "./router/trigger";
 
 const app = express();
 app.use(express.json());
@@ -10,5 +11,7 @@ app.use(cors())
 app.use("/api/v1/user", userRouter);
 
 app.use("/api/v1/zap", zapRouter);
+app.use("/api/v1/trigger", triggerRouter);
+app.use("/api/v1/action",actionRouter)
 
 app.listen(3000);
